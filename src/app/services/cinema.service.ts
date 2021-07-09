4564dd06feb02a74b5ac836024b3518d2184a3e8
kawtar-mnr <mounaouir.kawtar@gmail.com>
2021-07-09 12:53:32 +0100

    frontend

---
+++ src/app/services/cinema.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CinemaService {
+
+  public host:string="http://localhost:8080"
+  
+  constructor(private http:HttpClient) { }
+  public getVilles(){
+    return this.http.get(this.host+"/villes");
+  }
+  public getCinemas(v){
+    return this.http.get(v._links.cinemas.href);
+  }
+  public getSalles(c){
+    return this.http.get(c._links.salles.href);
+  }
+  public getProjections(salle){
+    let url=salle._links.projections.href.replace("{?projection}","");
+    return this.http.get(url+"?projection=p1");
+  }
+  public getTicketsPlaces(p){
+    let url=p._links.ticket.href.replace("{?projection}","");
+    return this.http.get(url+"?projection=ticketProj");
+  }
+  public payerTickets(dataForm){
+   return this.http.post(this.host+"/payerTickets",dataForm);
+  }
+}
